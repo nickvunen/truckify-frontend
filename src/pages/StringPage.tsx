5d87def3d69faebf59ad85ed83df05f1078739cf
Gavin <gavin.ligthart@gmail.com>
2025-05-09 21:16:35 +0200

    Add simple calendar

--- src/pages/StringPage.tsx
+++ src/pages/StringPage.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
+import Calendar from '../components/Calendar';
+
 const StringPage: React.FC = () => {
   const [result, setResult] = useState<string>("Loading...");
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch("http://localhost:8000/string")
+    fetch("http://localhost:8000/trucks")
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch");
         return res.json();
@@ -18,6 +20,7 @@ const StringPage: React.FC = () => {
     <div>
       <h2>String from Backend</h2>
       {error ? <p style={{ color: "red" }}>{error}</p> : <p>{result}</p>}
+      <Calendar />
     </div>
   );
 };
