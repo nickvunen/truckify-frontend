7881873ae65a89b27da6f36b4c613fe759a9aad0
Gavin <gavin.ligthart@gmail.com>
2025-05-09 19:25:10 +0200

    Move from react scripts to vite

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// filepath: /Users/gavinligthart/Sites/truckify-frontend/vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true
+  }
+});
