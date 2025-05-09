b0e90d610a272df7a3441e2d369ce09aa1b3ba8b
Gavin <gavin.ligthart@gmail.com>
2025-05-09 23:18:13 +0200

    Fix the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom"; // Use BrowserRouter instead of Router
+import { Routes, Route } from "react-router-dom"; // Use BrowserRouter instead of Router
 import Header from "./components/Header";
 import SideMenu from "./components/SideMenu";
 import CalendarPage from "./pages/CalendarPage";
@@ -7,18 +7,18 @@ import TrucksPage from "./pages/TrucksPage";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <div className="flex">
-          <SideMenu />
-          <div className="w-full p-8">
-            <CalendarPage />
-            <TrucksPage />
-          </div>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex">
+        <SideMenu />
+        <div className="w-full p-8">
+          <Routes>
+            <Route index element={<CalendarPage />} />
+            <Route path="trucks" element={<TrucksPage />} />
+          </Routes>
         </div>
       </div>
-    </Router>
+    </div>
   );
 };
 
